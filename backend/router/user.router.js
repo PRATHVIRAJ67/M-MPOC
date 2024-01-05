dba16040c6282f942bc2c50f9486da53eeb468db
roshan <roshan.suvarnkar@niveussolutions.com>
2024-01-05 15:20:37 +0530

    generalization, new route for center visualization

--- backend/router/user.router.js
+++ backend/router/user.router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const UserController = require('../controller/user.controller');
 
 router.get('/get-data', UserController.getData);
-router.get('/get-data-center-visualization', UserController.getData);
+router.get('/get-data-center-visualization', UserController.centerVisualization);
 
 
 
