65c72be545c35e143367de4afccb6b52e7d1e9ea
sowmitha-pai <msowmithapai2001@gmail.com>
2024-01-08 15:29:12 +0530

    new commit

--- frontend/src/components/Funnel.js
+++ frontend/src/components/Funnel.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 function Funnel() {
   return (
-    <div>Funnel</div>
-  )
+    <div className='funnel'>
+      <div className='funnel1'>
+        Conversion funnel analysis
+      </div>
+      <div classname = "funnel2">
+      <div className="dropdown">
+        <select
+          name="year"
+          id="years"
+        >
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+        </select>
+      </div>
+      <div className="more-vert-icon">
+        <MoreVertIcon />
+      </div>
+    </div>
+    </div>
+  );
 }
 
-export default Funnel
+export default Funnel;
